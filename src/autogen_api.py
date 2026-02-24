76e247108945b49bc6e1675b7c0647cb8d935272
Prabhu Selvamani <prabhuselvamani1995@gmail.com>
2026-02-24 21:21:50 +0530

    autogen_api.py completed

--- src/autogen_api.py
+++ src/autogen_api.py
@@ -1,6 +1,6 @@
 import os
-from autogen import AssistantAgent, UserProxyAgent
 from dotenv import load_dotenv
+from autogen import AssistantAgent, UserProxyAgent
 
 load_dotenv()
 
@@ -11,15 +11,15 @@ llm_config = {
         {
             "base_url": os.getenv("BASE_URL"),
             "api_key": os.getenv("OPENROUTER_API_KEY"),
-            "model": "nvidia/nemotron-nano-12b-v2-vl:free",
+            "model": os.getenv("MODEL_NAME"),
         }
     ]
 }
 
 assistant = AssistantAgent("assistant", llm_config=llm_config)
 user_proxy = UserProxyAgent("user_proxy", code_execution_config=False)
-
+#user=>agent=>llm=>agent
 user_proxy.initiate_chat(
     assistant,
-    message= "Tell me a joke on data scientists"
+    message= "Tell me a joke on data scientists" #user
 )
