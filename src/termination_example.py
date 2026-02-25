c6452dc68d8062fb9b5d82b9cd664340251c972d
Prabhu Selvamani <prabhuselvamani1995@gmail.com>
2026-02-25 20:55:09 +0530

    agentwise termination - max_consecutive_auto_reply=1

--- src/termination_example.py
+++ src/termination_example.py
@@ -27,14 +27,18 @@ agent2 = ConversableAgent(
     "sara",
     llm_config=llm_config,
     system_message="You are a duo standup comedian and you are good at telling jokes",
-    human_input_mode="NEVER"
+    human_input_mode="NEVER",
+    max_consecutive_auto_reply=2
     )
 
 agent2.initiate_chat(
     agent1,
     message="Tell me a one line quote on marcus aurelius",
-    max_turns=2
+    # max_turns=2
 )
 
 #initiate chat params - max_turns=1
+#agentwise 
+#1. max_consecutive_auto_reply=1
+#2. is_termination_msg
 
