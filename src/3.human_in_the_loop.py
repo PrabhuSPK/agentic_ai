25fd18425b1e40db4394b056b840f8e4a4cf6038
Prabhu Selvamani <prabhuselvamani1995@gmail.com>
2026-03-09 21:25:59 +0530

    Humanintheloop - human_input_mode=NEVER completed final code

--- src/3.human_in_the_loop.py
+++ src/3.human_in_the_loop.py
@@ -1,3 +1,5 @@
+############## Human in the loop: NEVER######################
+
 import os
 from dotenv import load_dotenv
 from autogen import AssistantAgent, ConversableAgent, UserProxyAgent
@@ -16,11 +18,25 @@ llm_config = {
     ]
 }
 
+agent1 = ConversableAgent(
+    "prabhu",
+    llm_config=llm_config,
+    system_message=(
+        "I have a number in my mind, and you will try to guess it. "
+        "If I say 'too high', you should guess a much lower number. "
+        "If I say 'high', you should guess a slightly lower number. "
+        "If I say 'too low', you should guess a much higher number. "
+        "If I say 'low', you should guess a slightly higher number. "
+        "Keep adjusting your guess based on the feedback until you get it right."
+    ),
+    human_input_mode="NEVER"
+    )
+
 agent2 = ConversableAgent(
     "sara",
     llm_config=llm_config,
     system_message=(
-        "You are playing a game of guess-my-number. You have the number 75 in your mind, "
+        "You are playing a game of guess-my-number. You have the number 65 in your mind, "
         "and I will try to guess it.\n"
         "If my guess is much higher than your number, say 'too high'.\n"
         "If my guess is much lower than your number, say 'too low'.\n"
@@ -29,20 +45,12 @@ agent2 = ConversableAgent(
         "If I guess correctly, say 'correct'."
     ),
     human_input_mode="NEVER",
-    is_termination_msg= lambda response:"75" in response["content"]
+    is_termination_msg= lambda response:"65" in response["content"]
     )
 
-## Human in the loop: ALWAYS
-human_proxy = ConversableAgent(
-    "human_proxy",
-    llm_config=False,  # no LLM used for human proxy
-    human_input_mode="ALWAYS",  # always ask for human input
-)
-
-human_proxy.initiate_chat(
-    agent2,
-    message="5",
-   
+agent2.initiate_chat(
+    agent1,
+    message="I have a number between 1 and 100. Guess it!",
 )
 
 
